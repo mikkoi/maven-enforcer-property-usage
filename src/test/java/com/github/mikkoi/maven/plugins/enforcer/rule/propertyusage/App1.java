e6f60fc8c08e22e517aacf30fd0cd7857253f11d
Mikko Koivunalho <mikko.koivunalho@seb.se>
2018-10-09 09:18:59 +0200

    Make variables local

--- src/test/java/com/github/mikkoi/maven/plugins/enforcer/rule/propertyusage/App1.java
+++ src/test/java/com/github/mikkoi/maven/plugins/enforcer/rule/propertyusage/App1.java
@@ -8,16 +8,12 @@ import java.util.Properties;
 @SuppressWarnings({"all"})
 public class App1 {
 
-    private String myPropertyValue = "";
-
-    private Integer otherPropVal = 0;
-
     private App1() {
         Properties properties = new Properties();
         try (InputStream inputStream = new FileInputStream("app1.properties") ) {
             properties.load(inputStream);
-            myPropertyValue = properties.getProperty("my.property.value");
-            otherPropVal = Integer.valueOf(properties.getProperty("other.prop.val"));
+            final String myPropertyValue = properties.getProperty("my.property.value");
+            final Integer otherPropVal = Integer.valueOf(properties.getProperty("other.prop.val"));
 
         } catch (IOException e) {
             System.out.print(e.getMessage());
