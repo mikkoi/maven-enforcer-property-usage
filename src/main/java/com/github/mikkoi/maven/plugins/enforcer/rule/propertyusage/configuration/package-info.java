114d699545053b15d9cb98b85fbcb18c0f019349
Mikko Koivunalho <mikko.koivunalho@seb.se>
2018-10-10 09:13:13 +0200

    Add package-info.java to c.g.m.m.p.e.r.propertyusage.configuration

---
+++ src/main/java/com/github/mikkoi/maven/plugins/enforcer/rule/propertyusage/configuration/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The Maven Enforcer rule Property Usage.
+ */
+@ParametersAreNonnullByDefault
+package com.github.mikkoi.maven.plugins.enforcer.rule.propertyusage.configuration;
+
+import javax.annotation.ParametersAreNonnullByDefault;
